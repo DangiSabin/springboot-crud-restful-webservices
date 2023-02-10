94c91b88e36d019a13148ad3fa6c7062052efdf1
Sabin Dangi <sabin4.dangi@gmail.com>
2023-02-09 22:44:37 -0800

    first commit

---
+++ src/main/java/com/sabindangi/springbootcrudrestfulwebservices/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sabindangi.springbootcrudrestfulwebservices.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sabindangi.springbootcrudrestfulwebservices.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+
+}
